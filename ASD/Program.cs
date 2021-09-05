c1b41da2f6c1a6e5adef51a971d0c030c56b03f2
BuildTools <unconfigured@null.spigotmc.org>
2021-09-05 17:16:03 +0300

    Add lab_1

--- ASD/Program.cs
+++ ASD/Program.cs
@@ -2,11 +2,17 @@
 
 namespace ASD
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      
+        public static void Main(string[] args)
+        {
+            double a = 2, b = 0.5, x, Y, F;
+            Console.Write("Введите x... ");
+            x = Double.Parse(Console.ReadLine());
+            Y = Math.Pow(x, 2) * (Math.Pow(Math.Atan(a * x + b), 2) - Math.Sqrt(Math.Abs(Math.Pow(x - a, 2) - b)));
+            F = Math.Sqrt(a * Math.Pow(x, 2) + b + x) - Math.Pow(Math.E, -a + x) + a * x * Math.Log(x + a - 5);
+            Console.WriteLine($"Y = {Y}, F = {F}");
+            Console.ReadLine();
+        }
     }
-  }
 }
