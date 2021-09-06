b3a691bf084279d250b66fbe604a425cfd9d0a77
BuildTools <unconfigured@null.spigotmc.org>
2021-09-06 11:28:45 +0300

    Finish Task_1

--- Task_1/Form1.Designer.cs
+++ Task_1/Form1.Designer.cs
@@ -29,11 +29,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(31, 102);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 2;
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(169, 102);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.ReadOnly = true;
+            this.textBox21.Size = new System.Drawing.Size(100, 20);
+            this.textBox21.TabIndex = 3;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(113, 62);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 4;
+            this.button.Text = "Change";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(313, 153);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Task_1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.Button button;
 
         #endregion
     }
--- Task_1/Form1.cs
+++ Task_1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task_1
@@ -15,6 +8,27 @@ namespace Task_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(textBox1.Text) <= Convert.ToInt32(textBox2.Text))
+                {
+                    textBox11.Text = Convert.ToString(0);
+                    textBox21.Text = textBox2.Text;
+                }
+                else
+                {
+                    textBox11.Text = textBox1.Text;
+                    textBox21.Text = textBox2.Text;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введено не число");
+            }
         }
     }
 }
