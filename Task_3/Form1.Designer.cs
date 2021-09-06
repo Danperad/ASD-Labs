2a5230d46b56b499935eb5008d2e482121aec2b3
BuildTools <unconfigured@null.spigotmc.org>
2021-09-06 12:36:36 +0300

    Add Task_3

--- Task_3/Form1.Designer.cs
+++ Task_3/Form1.Designer.cs
@@ -29,11 +29,384 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.radioButton12 = new System.Windows.Forms.RadioButton();
+            this.radioButton13 = new System.Windows.Forms.RadioButton();
+            this.radioButton14 = new System.Windows.Forms.RadioButton();
+            this.radioButton24 = new System.Windows.Forms.RadioButton();
+            this.radioButton23 = new System.Windows.Forms.RadioButton();
+            this.radioButton22 = new System.Windows.Forms.RadioButton();
+            this.radioButton21 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton34 = new System.Windows.Forms.RadioButton();
+            this.radioButton33 = new System.Windows.Forms.RadioButton();
+            this.radioButton32 = new System.Windows.Forms.RadioButton();
+            this.radioButton31 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton44 = new System.Windows.Forms.RadioButton();
+            this.radioButton43 = new System.Windows.Forms.RadioButton();
+            this.radioButton42 = new System.Windows.Forms.RadioButton();
+            this.radioButton41 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox51 = new System.Windows.Forms.CheckBox();
+            this.checkBox52 = new System.Windows.Forms.CheckBox();
+            this.checkBox53 = new System.Windows.Forms.CheckBox();
+            this.checkBox54 = new System.Windows.Forms.CheckBox();
+            this.Check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Тест \"Компьютерные сети\"";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "E-mail - это:";
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.Location = new System.Drawing.Point(12, 72);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(141, 24);
+            this.radioButton11.TabIndex = 2;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "Поисковая программа";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            this.radioButton11.CheckedChanged += new System.EventHandler(this.radioButton11_CheckedChanged);
+            // 
+            // radioButton12
+            // 
+            this.radioButton12.Location = new System.Drawing.Point(12, 102);
+            this.radioButton12.Name = "radioButton12";
+            this.radioButton12.Size = new System.Drawing.Size(176, 24);
+            this.radioButton12.TabIndex = 3;
+            this.radioButton12.TabStop = true;
+            this.radioButton12.Text = "Название почтового сервера";
+            this.radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            this.radioButton13.Location = new System.Drawing.Point(12, 132);
+            this.radioButton13.Name = "radioButton13";
+            this.radioButton13.Size = new System.Drawing.Size(141, 24);
+            this.radioButton13.TabIndex = 4;
+            this.radioButton13.TabStop = true;
+            this.radioButton13.Text = "Почтовая программа";
+            this.radioButton13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14
+            // 
+            this.radioButton14.Location = new System.Drawing.Point(12, 162);
+            this.radioButton14.Name = "radioButton14";
+            this.radioButton14.Size = new System.Drawing.Size(216, 38);
+            this.radioButton14.TabIndex = 5;
+            this.radioButton14.TabStop = true;
+            this.radioButton14.Text = "Обмен письмами в компьютерных сетях(электронная почта)";
+            this.radioButton14.UseVisualStyleBackColor = true;
+            // 
+            // radioButton24
+            // 
+            this.radioButton24.Location = new System.Drawing.Point(12, 325);
+            this.radioButton24.Name = "radioButton24";
+            this.radioButton24.Size = new System.Drawing.Size(104, 24);
+            this.radioButton24.TabIndex = 10;
+            this.radioButton24.TabStop = true;
+            this.radioButton24.Text = "Провайдеры";
+            this.radioButton24.UseVisualStyleBackColor = true;
+            // 
+            // radioButton23
+            // 
+            this.radioButton23.Location = new System.Drawing.Point(12, 295);
+            this.radioButton23.Name = "radioButton23";
+            this.radioButton23.Size = new System.Drawing.Size(141, 24);
+            this.radioButton23.TabIndex = 9;
+            this.radioButton23.TabStop = true;
+            this.radioButton23.Text = "Телеконференции";
+            this.radioButton23.UseVisualStyleBackColor = true;
+            // 
+            // radioButton22
+            // 
+            this.radioButton22.Location = new System.Drawing.Point(12, 265);
+            this.radioButton22.Name = "radioButton22";
+            this.radioButton22.Size = new System.Drawing.Size(104, 24);
+            this.radioButton22.TabIndex = 8;
+            this.radioButton22.TabStop = true;
+            this.radioButton22.Text = "Браузеры";
+            this.radioButton22.UseVisualStyleBackColor = true;
+            // 
+            // radioButton21
+            // 
+            this.radioButton21.Location = new System.Drawing.Point(12, 235);
+            this.radioButton21.Name = "radioButton21";
+            this.radioButton21.Size = new System.Drawing.Size(141, 24);
+            this.radioButton21.TabIndex = 7;
+            this.radioButton21.TabStop = true;
+            this.radioButton21.Text = "Поисковые серверы";
+            this.radioButton21.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Для просмотра WEB-страниц предназначены:";
+            // 
+            // radioButton34
+            // 
+            this.radioButton34.Location = new System.Drawing.Point(290, 162);
+            this.radioButton34.Name = "radioButton34";
+            this.radioButton34.Size = new System.Drawing.Size(104, 24);
+            this.radioButton34.TabIndex = 15;
+            this.radioButton34.TabStop = true;
+            this.radioButton34.Text = "10,0,0,1225";
+            this.radioButton34.UseVisualStyleBackColor = true;
+            // 
+            // radioButton33
+            // 
+            this.radioButton33.Location = new System.Drawing.Point(290, 132);
+            this.radioButton33.Name = "radioButton33";
+            this.radioButton33.Size = new System.Drawing.Size(104, 24);
+            this.radioButton33.TabIndex = 14;
+            this.radioButton33.TabStop = true;
+            this.radioButton33.Text = "1278.214.198";
+            this.radioButton33.UseVisualStyleBackColor = true;
+            // 
+            // radioButton32
+            // 
+            this.radioButton32.Location = new System.Drawing.Point(290, 102);
+            this.radioButton32.Name = "radioButton32";
+            this.radioButton32.Size = new System.Drawing.Size(104, 24);
+            this.radioButton32.TabIndex = 13;
+            this.radioButton32.TabStop = true;
+            this.radioButton32.Text = "18.274.198.0";
+            this.radioButton32.UseVisualStyleBackColor = true;
+            // 
+            // radioButton31
+            // 
+            this.radioButton31.Location = new System.Drawing.Point(290, 72);
+            this.radioButton31.Name = "radioButton31";
+            this.radioButton31.Size = new System.Drawing.Size(104, 24);
+            this.radioButton31.TabIndex = 12;
+            this.radioButton31.TabStop = true;
+            this.radioButton31.Text = "108.214.198.112";
+            this.radioButton31.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(290, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 27);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Выберите корректный IP-адрес компьютера в сети";
+            // 
+            // radioButton44
+            // 
+            this.radioButton44.Location = new System.Drawing.Point(290, 325);
+            this.radioButton44.Name = "radioButton44";
+            this.radioButton44.Size = new System.Drawing.Size(216, 33);
+            this.radioButton44.TabIndex = 20;
+            this.radioButton44.TabStop = true;
+            this.radioButton44.Text = "Запуска программы с удаленного компьютера";
+            this.radioButton44.UseVisualStyleBackColor = true;
+            // 
+            // radioButton43
+            // 
+            this.radioButton43.Location = new System.Drawing.Point(290, 295);
+            this.radioButton43.Name = "radioButton43";
+            this.radioButton43.Size = new System.Drawing.Size(216, 24);
+            this.radioButton43.TabIndex = 19;
+            this.radioButton43.TabStop = true;
+            this.radioButton43.Text = "Управления передачи сообщениями";
+            this.radioButton43.UseVisualStyleBackColor = true;
+            // 
+            // radioButton42
+            // 
+            this.radioButton42.Location = new System.Drawing.Point(290, 265);
+            this.radioButton42.Name = "radioButton42";
+            this.radioButton42.Size = new System.Drawing.Size(119, 24);
+            this.radioButton42.TabIndex = 18;
+            this.radioButton42.TabStop = true;
+            this.radioButton42.Text = "Передачи файлов";
+            this.radioButton42.UseVisualStyleBackColor = true;
+            // 
+            // radioButton41
+            // 
+            this.radioButton41.Location = new System.Drawing.Point(290, 235);
+            this.radioButton41.Name = "radioButton41";
+            this.radioButton41.Size = new System.Drawing.Size(153, 24);
+            this.radioButton41.TabIndex = 17;
+            this.radioButton41.TabStop = true;
+            this.radioButton41.Text = "Передачи гипертекста";
+            this.radioButton41.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(290, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 23);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Протокол HTTP служит для:";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(544, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 35);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Существуют такие топологии сети как:";
+            // 
+            // checkBox51
+            // 
+            this.checkBox51.Location = new System.Drawing.Point(540, 146);
+            this.checkBox51.Name = "checkBox51";
+            this.checkBox51.Size = new System.Drawing.Size(104, 24);
+            this.checkBox51.TabIndex = 22;
+            this.checkBox51.Text = "Кольцо";
+            this.checkBox51.UseVisualStyleBackColor = true;
+            // 
+            // checkBox52
+            // 
+            this.checkBox52.Location = new System.Drawing.Point(540, 176);
+            this.checkBox52.Name = "checkBox52";
+            this.checkBox52.Size = new System.Drawing.Size(104, 24);
+            this.checkBox52.TabIndex = 23;
+            this.checkBox52.Text = "Звезда";
+            this.checkBox52.UseVisualStyleBackColor = true;
+            // 
+            // checkBox53
+            // 
+            this.checkBox53.Location = new System.Drawing.Point(540, 206);
+            this.checkBox53.Name = "checkBox53";
+            this.checkBox53.Size = new System.Drawing.Size(104, 24);
+            this.checkBox53.TabIndex = 24;
+            this.checkBox53.Text = "Прямая";
+            this.checkBox53.UseVisualStyleBackColor = true;
+            // 
+            // checkBox54
+            // 
+            this.checkBox54.Location = new System.Drawing.Point(540, 235);
+            this.checkBox54.Name = "checkBox54";
+            this.checkBox54.Size = new System.Drawing.Size(104, 24);
+            this.checkBox54.TabIndex = 25;
+            this.checkBox54.Text = "Жираф";
+            this.checkBox54.UseVisualStyleBackColor = true;
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(613, 325);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 26;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.checkBox54);
+            this.Controls.Add(this.checkBox53);
+            this.Controls.Add(this.checkBox52);
+            this.Controls.Add(this.checkBox51);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton44);
+            this.Controls.Add(this.radioButton43);
+            this.Controls.Add(this.radioButton42);
+            this.Controls.Add(this.radioButton41);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton34);
+            this.Controls.Add(this.radioButton33);
+            this.Controls.Add(this.radioButton32);
+            this.Controls.Add(this.radioButton31);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton24);
+            this.Controls.Add(this.radioButton23);
+            this.Controls.Add(this.radioButton22);
+            this.Controls.Add(this.radioButton21);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton14);
+            this.Controls.Add(this.radioButton13);
+            this.Controls.Add(this.radioButton12);
+            this.Controls.Add(this.radioButton11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Task_3";
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.CheckBox checkBox54;
+
+        private System.Windows.Forms.CheckBox checkBox53;
+
+        private System.Windows.Forms.CheckBox checkBox51;
+        private System.Windows.Forms.CheckBox checkBox52;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.RadioButton radioButton44;
+
+        private System.Windows.Forms.RadioButton radioButton43;
+
+        private System.Windows.Forms.RadioButton radioButton42;
+
+        private System.Windows.Forms.RadioButton radioButton41;
+
+        private System.Windows.Forms.Button Check;
+
+        private System.Windows.Forms.RadioButton radioButton34;
+
+        private System.Windows.Forms.RadioButton radioButton33;
+
+        private System.Windows.Forms.RadioButton radioButton32;
+
+        private System.Windows.Forms.RadioButton radioButton31;
+
+        private System.Windows.Forms.RadioButton radioButton24;
+
+        private System.Windows.Forms.RadioButton radioButton23;
+
+        private System.Windows.Forms.RadioButton radioButton22;
+
+        private System.Windows.Forms.RadioButton radioButton21;
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.RadioButton radioButton12;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton13;
+        private System.Windows.Forms.RadioButton radioButton14;
+        private System.Windows.Forms.RadioButton radioButton15;
+        private System.Windows.Forms.RadioButton radioButton16;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
 
         #endregion
     }
--- Task_3/Form1.cs
+++ Task_3/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task_3
@@ -15,6 +8,16 @@ namespace Task_3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void radioButton11_CheckedChanged(object sender, EventArgs e)
+        {
+            rad
         }
     }
 }
