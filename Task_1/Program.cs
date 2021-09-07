f24d2372622e5d724285c679592fafd95c3b3a50
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 15:23:58 +0300

    Finish Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,7 +6,22 @@ namespace Task_1
   {
     public static void Main(string[] args)
     {
-      
+      const int n = 32;
+      int[] A = new int[n];
+      Console.WriteLine($"Введите {n} чисел");
+      for (int i = 0; i < n; i++)
+      {
+        A[i] = Int32.Parse(Console.ReadLine());
+      }
+
+      int m = 0, nul = 0;
+      for(int i = 0; i < n; i++)
+      {
+        if (A[i] < 0 && i < 20) m++;
+        else if (i == 0) nul++;
+      }
+      Console.WriteLine($"Количество отрицательных: {m}, Количество нулей {nul}");
+      Console.ReadKey();
     }
   }
 }
