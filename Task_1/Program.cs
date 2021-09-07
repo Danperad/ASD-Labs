46f434105a2664c3b5cb757d9e6918fa13bee193
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 13:40:16 +0300

    Finish Lab_6

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,11 +2,39 @@
 
 namespace Task_1
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      
+        public static void Main(string[] args)
+        {
+            const int n = 11;
+            int[] A = new int[n];
+            Console.WriteLine($"Введите {n} чисел");
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = Int32.Parse(Console.ReadLine());
+            }
+            int min = Int32.MaxValue,minI = -1, max = Int32.MinValue, maxI = -1;
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] > max)
+                {
+                    max = A[i];
+                    maxI = i;
+                }
+                if (A[i] < min)
+                {
+                    min = A[i];
+                    minI = i;
+                }
+            }
+            if (minI != -1) A[minI] = -1;
+            if (maxI != -1) A[maxI] = 1;
+            foreach (var i in A)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.ReadKey();
+        }
     }
-  }
 }
---
+++ Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int n = 20;
+            int[] A = new int[n], B = new int[n], C = new int[n];
+            Console.WriteLine($"Введите {n} чисел для массива A");
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Введите {n} чисел для массива B");
+            for (int i = 0; i < n; i++)
+            {
+                B[i] = Int32.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                C[i] = Math.Min(A[i], B[i]);
+            }
+            foreach (var i in C)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
