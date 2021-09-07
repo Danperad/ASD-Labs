f685dd14cf8999a187cd47e558b75927033e393c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 12:56:28 +0300

    Finish Lab_3

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,7 +6,13 @@ namespace Task_1
   {
     public static void Main(string[] args)
     {
-      
+      for (double i = 1; i <= 5; i+=0.4)
+      {
+        double y = Math.Log(i * i) + 3 * i;
+        Console.WriteLine($"При x = {i}, y = {y}");
+      }
+
+      Console.ReadKey();
     }
   }
 }
---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, max = 1;
+            Console.WriteLine("Введите 2 числа");
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+            for (int i = 2; i <= Math.Min(a, b); i++)
+            {
+                if (a % i == 0 && b % i == 0) max = i;
+            }
+            Console.WriteLine($"НОД для {a} и {b} - {max}");
+            Console.ReadKey();
+        }
+    }
+}
