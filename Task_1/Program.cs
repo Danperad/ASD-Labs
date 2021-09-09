8b8698f02cd8e753247d0f84e96e9ebb51e04765
BuildTools <unconfigured@null.spigotmc.org>
2021-09-09 10:13:53 +0300

    Finish Lab_4

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,7 +6,33 @@ namespace Task_1
   {
     public static void Main(string[] args)
     {
-      
+      string word = "СТУДЕНТ";
+      for (int i = 0; i < 6; i++)
+      {
+        switch (i)
+        {
+          case 0:
+            Console.WriteLine("Именительный: "+word);
+            break;
+          case 1:
+            Console.WriteLine("Родительный: "+word+"А");
+            break;
+          case 2:
+            Console.WriteLine("Дательный: "+word+"У");
+            break;
+          case 3:
+            Console.WriteLine("Винительный: "+word+"А");
+            break;
+          case 4:
+            Console.WriteLine("Творительный: "+word+"ОМ");
+            break;
+          case 5:
+            Console.WriteLine("Предложный: "+"О "+word+"Е");
+            break;
+        }
+      }
+
+      Console.ReadKey();
     }
   }
 }
