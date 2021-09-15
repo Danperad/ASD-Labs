c2954a602ac0819c05ce0d7ea5205b225cd2005e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 17:42:51 +0300

    Add Lab_5

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите V");
+            double V = Double.Parse(Console.ReadLine()), S = 0, x = 10;
+            ulong n = 0, r;
+            do
+            {
+                r = 1;
+                n++;
+                for (ulong i = 1; i <= n + 2; i++) r *= i;
+                x = x * (n + 1.0) / r;
+                S = S + x;
+            } while (S < V);
+            Console.WriteLine($"{n}");
+            Console.ReadLine();
+        }
+    }
+}
