2bde878a3e1d77ada63111da18d64918f47a9982
BuildTools <unconfigured@null.spigotmc.org>
2021-09-16 12:21:38 +0300

    Rework Lab

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,37 +2,21 @@
 
 namespace Task_1
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      string word = "СТУДЕНТ";
-      for (int i = 0; i < 6; i++)
-      {
-        switch (i)
+        public static void Main(string[] args)
         {
-          case 0:
-            Console.WriteLine("Именительный: "+word);
-            break;
-          case 1:
-            Console.WriteLine("Родительный: "+word+"А");
-            break;
-          case 2:
-            Console.WriteLine("Дательный: "+word+"У");
-            break;
-          case 3:
-            Console.WriteLine("Винительный: "+word+"А");
-            break;
-          case 4:
-            Console.WriteLine("Творительный: "+word+"ОМ");
-            break;
-          case 5:
-            Console.WriteLine("Предложный: "+"О "+word+"Е");
-            break;
+            string word = "СТУДЕНТ";
+            for (int i = 0; i < 6; i++)
+            {
+                if (i == 0) Console.WriteLine("Именительный: " + word);
+                else if (i == 1) Console.WriteLine("Родительный: " + word + "А");
+                else if (i == 2) Console.WriteLine("Дательный: " + word + "У");
+                else if (i == 3) Console.WriteLine("Винительный: " + word + "А");
+                else if (i == 4) Console.WriteLine("Творительный: " + word + "ОМ");
+                else if (i == 5) Console.WriteLine("Предложный: " + "О " + word + "Е");
+            }
+            Console.ReadKey();
         }
-      }
-
-      Console.ReadKey();
     }
-  }
 }
